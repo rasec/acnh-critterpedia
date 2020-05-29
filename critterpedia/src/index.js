09da03c3fb1dafc4b56dbef49d45720be6916160
Cesar <cesar.cruz@accedo.tv>
2020-05-29 22:02:02 +0200

    Added all fishes with images and place filter

--- critterpedia/src/index.js
+++ critterpedia/src/index.js
@@ -1,23 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import fishes from './data/fishes.json';
-
-const rows = (fishes.length % 5);
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
+import './index.scss';
+import fishesData from './data/fishes.json';
+import places from './data/places.json';
 
 class CritterPediaTable extends React.Component {
-  renderCell(critter) {
-    return <CritterPediaCell critter={critter} />
+  constructor(props) {
+    super(props);
+    this.selectItem = this.selectItem.bind(this);
+    this.state = {
+      fishes: fishesData,
+    };
+  }
+  selectItem(id) {
+    const fishes = fishesData.filter(fish => (fish.place === id));
+    this.setState({
+      fishes
+    });
+  }
+  renderCell(critter, place) {
+    return <CritterPediaCell key={critter.id} critter={critter} place={place} />
   }
   render() {
     return (
       <div className="critterpedia">
+        <PlaceSelector selectItem={this.selectItem} />
         <div className="critterpedia-grid">
           {(() => {
-            return fishes.map((fish) => {
-              return this.renderCell(fish)
-            })
-          })()}
+            return this.state.fishes.map((fish) => {
+              const fishPlace = places.find(place => (fish.place === place.id));
+              return this.renderCell(fish, fishPlace);
+            });
+          })()
+          }
         </div>
       </div >
     );
@@ -26,13 +43,46 @@ class CritterPediaTable extends React.Component {
 
 class CritterPediaCell extends React.Component {
   render() {
-    return (<div className="critterpedia-cell" id={this.props.critter.id}><img src={require(`./data/images/fish${this.props.critter.id}.png`)} /></div>);
+    return (<div className="critterpedia-cell" id={this.props.critter.id}><img src={require(`./data/images/fish${this.props.critter.id}.png`)} />{this.props.place.name}</div>);
   }
 }
 
 class CritterPediaItem extends React.Component {
   render() {
     return (<div className="critterpedia-item"></div>);
+  }
+}
+
+class PlaceSelector extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedItem: 'Select a place',
+    };
+  }
+  selectItem(id, name) {
+    this.setState({
+      selectedItem: name
+    });
+    this.props.selectItem(id)
+  }
+  renderSelectorItem(id, name, selectItem) {
+    return (
+      <Dropdown.Item key={id} id={id} onSelect={() => { this.selectItem(id, name) }}>{name}</Dropdown.Item>
+    );
+  }
+  render() {
+
+    return (
+      <DropdownButton variant="info" title={this.state.selectedItem}>
+        {(() => {
+          return places.map(({ id, name }) => {
+            return this.renderSelectorItem(id, name);
+          });
+        })()
+        }
+      </DropdownButton>
+    );
   }
 }
 
